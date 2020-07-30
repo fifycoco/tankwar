f0a791d46d1f892e41175fd8b088be8c29da3def
fifycoco <fifycoco@gmail.com>
2020-07-30 20:34:37 +0800

    移動坦克測試

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -1,28 +1,79 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class GameClient extends JComponent {
 
-    private int swidth;
-    private int sheight;
+    private int screenwidth;
+    private int screenheight;
 
-    GameClient(){
+    private Tank playerTank;
+
+    GameClient() {
         //this.setPreferredSize(new Dimension(800,600));
-        this(800,600);
+        this(800, 600);
     }
 
-    GameClient(int swidth, int sheight){
-        this.swidth = swidth;
-        this.sheight = sheight;
-        this.setPreferredSize(new Dimension(swidth, sheight));
+    GameClient(int screenwidth, int screenheight) {
+        this.screenwidth = screenwidth;
+        this.screenheight = screenheight;
+        this.setPreferredSize(new Dimension(screenwidth, screenheight));
+        // initial
+        init();
+    }
+
+    public void init() {
+        // 指定坦克圖形 by Direction
+        playerTank = new Tank(getCenterPosX(50), getCenterPosY(50), Direction.DOWN);
+
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         //super.paintComponent(g);
-        g.drawImage(new ImageIcon("assets/images/itankD.png").getImage(),400,100,null);
+        // 指定圖形, 指定XY
+        //g.drawImage(new ImageIcon("assets/images/itankD.png").getImage(),400,100,null);
+
+        // 指定圖形, 螢幕中央點
+        //g.drawImage(new ImageIcon("assets/images/itankD.png").getImage(),getCenterPosX(100),getCenterPosY(100),null);
+
+        // payerTank.getImage 取坦克圖形
+        g.drawImage(playerTank.getImage(), playerTank.getX(), playerTank.getY(), null);
 
 
+    }
 
+    // X bar 中點
+    private int getCenterPosX(int width) {
+        return (screenwidth - width) / 2;
+    }
+
+    // Y bar 中點
+    private int getCenterPosY(int height) {
+        return (screenheight - height) / 2;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                playerTank.setDirection(Direction.UP);
+                playerTank.setY(playerTank.getY() - 5);
+                break;
+            case KeyEvent.VK_DOWN:
+                playerTank.setDirection(Direction.DOWN);
+                playerTank.setY(playerTank.getY() + 5);
+                break;
+            case KeyEvent.VK_LEFT:
+                playerTank.setDirection(Direction.LEFT);
+                playerTank.setX(playerTank.getX() - 5);
+                break;
+            case KeyEvent.VK_RIGHT:
+                playerTank.setDirection(Direction.RIGHT);
+                playerTank.setX(playerTank.getX() + 5);
+                break;
+            default:;
+        }
+        // call paintComponent method
+        this.repaint();
     }
 }
--- src/main/java/TankGame.java
+++ src/main/java/TankGame.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class TankGame {
     public static void main(String[] args) {
@@ -15,6 +17,26 @@ public class TankGame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         gameClient.repaint();
+
+        // 按鍵偵測
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                //super.keyPressed(e);
+                //System.out.println((char)e.getKeyCode());
+
+                // 移動坦克
+                gameClient.keyPressed(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+            }
+        });
+
+
+
 
     }
 }
